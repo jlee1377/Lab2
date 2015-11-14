aa32296a55bdc25d709412dd32bd48aa2c740e91
Jaylan Tse <dry1ce2223@gmail.com>
2015-11-14 00:42:10 -0800

    Form fragment created. Hope I did this correctly.

---
+++ app/src/main/java/com/group5/lab2/FormFragment.java
@@ -0,0 +1,67 @@
+package com.group5.lab2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class FormFragment extends Fragment {
+
+    private EditText homeValueInput;
+    private EditText downPaymentInput;
+    private EditText interestRateInput;
+    private EditText propertyTaxRateInput;
+    private Spinner termsSpinner;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_form, container, false);
+
+        homeValueInput = (EditText) view.findViewById(R.id.input_home_value);
+        downPaymentInput = (EditText) view.findViewById(R.id.input_down_payment);
+        interestRateInput = (EditText) view.findViewById(R.id.input_interest_rate);
+        propertyTaxRateInput = (EditText) view.findViewById(R.id.input_property_tax_rate);
+
+        termsSpinner = (Spinner) view.findViewById(R.id.spinner_terms);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.terms_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        termsSpinner.setAdapter(adapter);
+
+        Button calculateBtn = (Button) view.findViewById(R.id.btn_calculate);
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate();
+            }
+        });
+
+        Button resetBtn = (Button) view.findViewById(R.id.btn_reset);
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
+
+        return view;
+    }
+
+    private void calculate() {
+
+    }
+
+    private void reset() {
+        homeValueInput.setText("");
+        downPaymentInput.setText("");
+        interestRateInput.setText("");
+        propertyTaxRateInput.setText("");
+        termsSpinner.setSelection(0);
+    }
+}
--- app/src/main/java/com/group5/lab2/MainActivity.java
+++ app/src/main/java/com/group5/lab2/MainActivity.java
@@ -2,12 +2,23 @@ package com.group5.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
